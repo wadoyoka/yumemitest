e034b2823fd52a638d63e5c87e5b58895cf88807
Wadoyoka <atsushi021028@gmail.com>
2025-02-24 01:18:56 +0900

    Delete: SelectDisplayItemComponent & Rechats Graph

--- components/layout/PopulationGraph/PopulationGraph.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import type { PopulationGraphProps } from '@/types/Population/Population'
-import { PopulationGraphView } from './PopulationGraphView'
-
-export default function PopulationGraph({ data }: PopulationGraphProps) {
-  return <PopulationGraphView data={data} />
-}
